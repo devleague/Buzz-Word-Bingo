2ed1da2ba5677a7a8efbffb4dc558fd8d8b3ccf0
Audrey <audreybrown94@gmail.com>
2018-03-17 10:58:45 -1000

    all routes functional

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ var app = express();
 var bodyParser = require("body-parser");
 var jsonParser = bodyParser.json();
 const PORT = 8080;
-let buzzWords = {};
+let buzzWords = [];
 
 app.use(
   express.static("/public", next => {
@@ -24,17 +24,25 @@ app.get("/buzzwords", (req, res) => {
 
 app.post("/buzzwords", (req, res) => {
   let newWord = req.body;
-
-  if (Object.keys(buzzWords).length > 10) {
+  //   for (var i = 0; i < buzzWords.length; i++) {
+  //     //if buzzWord already exists in array, overwite
+  //     if (buzzWords[i].buzzWord === newWord.buzzWord) {
+  //       buzzWords[i].buzzWord = newWord.buzzWord;
+  //       buzzWords[i].points = newWord.points;
+  //       res.end();
+  //       return buzzWords;
+  //     }
+  //   }
+  if (buzzWords.length > 5) {
     //err if more than 5 objects
     res.append("success", "false").end();
   } else {
-    res.append("success", "true");
-    buzzWords.buzzWord = newWord.buzzWord; //adding to object
-    buzzWords.points = newWord.points;
-    console.log(buzzWords);
-
+    buzzWords.push({
+      buzzWord: newWord.buzzWord,
+      points: newWord.points
+    });
     res.end();
+    console.log(buzzWords);
     return buzzWords;
   }
 });
@@ -42,19 +50,60 @@ app.post("/buzzwords", (req, res) => {
 app.put("/buzzwords", (req, res) => {
   let putWord = req.body;
 
-  for (var i = 0; i <= Object.keys(buzzWords).length; i++) {
+  for (var i = 0; i <= buzzWords.length; i++) {
     //iterating to find matching key
-
-    if (buzzWords.buzzWord[i] === putWord.buzzWord) {
-      res.append("success", "true");
-      buzzWords.putWord[i] = putWord.points;
+    if (buzzWords[i].buzzWord === putWord.buzzWord) {
+      //   res.send("success: true");
+      buzzWords[i].points = Number(putWord.points);
       console.log(buzzWords);
 
       res.end();
+      return buzzWords;
     } else {
-      res.append("success", "false").end();
+      //   res.send({"success: false"});
+      res.end();
     }
   }
 });
 
+app.delete("/buzzwords", (req, res) => {
+  let deletedWord = req.body;
+
+  for (var i = 0; i <= buzzWords.length; i++) {
+    if (buzzWords[i].buzzWord === deletedWord.buzzWord) {
+      buzzWords.splice(i, 1);
+      console.log(buzzWords);
+      res.end();
+      return buzzWords;
+    }
+  }
+});
+
+app.post("/reset", (req, res) => {
+  buzzWords = [];
+  res.end();
+  return buzzWords;
+});
+app.post("/heard", (req, res) => {
+  let heardWord = req.body;
+  for (var i = 0; i <= buzzWords.length; i++) {
+    if ((buzzWords[i].buzzWord = heardWord.buzzWord)) {
+      //chosen word
+      let addScore = Number(buzzWords[i].points);
+      console.log(buzzWords);
+      if (buzzWords[i].totalScore) {
+        //if exists already, update totalScore
+        buzzWords[i].totalScore = Number(addScore + buzzWords[i].totalScore);
+        res.end();
+        console.log(buzzWords);
+        return buzzWords;
+      } else {
+        buzzWords[i].totalScore = addScore;
+        res.end();
+        console.log(buzzWords);
+        return buzzWords;
+      }
+    }
+  }
+});
 app.listen(PORT, () => console.log("Server listening on port 8080"));
